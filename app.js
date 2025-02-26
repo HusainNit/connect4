943b6c607c703180d546e82e8259bd31f6cdc233
Husain Ali <vaintime21@email.com>
2025-02-27 00:00:18 +0300

    try#2.1

--- app.js
+++ app.js
@@ -13,37 +13,38 @@ let board=[
 ]
 
 const turns=['red','blue']
-
 let currentTurn='red';
 
-const test =(event)=>{
-console.log(`press ${event.target.id}`);
-// console.log(document.querySelector(`#${event.target.id}));//dont works
-    let getId = document.getElementById(event.target.id);
-    let spletId=getId.id.split('-');
-    let column=spletId[1];
-    let row=spletId[0];
-    console.log(`row:${row}  column:${column}`);
+const init=()=>{
+    currentTurn ='red';
+    for(let row=0;row<board.length;row++){
+        for(let column=0;column<board[row].length;column++){
+            board[row][column]='';
+        }
+    }
+}
+init()
 
-   
+const handelPlay=(event)=>{
+    console.log(`press ${event.target.id}`);
+    // console.log(document.querySelector(`#${event.target.id}));//dont works
+    let getId = document.getElementById(event.target.id);
+    let splitId=getId.id.split('-');
+    let column=splitId[1];
+    let row=splitId[0];
+    console.log(`row:${row}  column:${column}`);
 
     for(let rows= board.length-1; rows >= 0; rows--) {
         if(board[rows][column-1] === ''){
             board[rows][column-1] =currentTurn;
-            console.log(document.getElementById(`${rows}-${column}`));
+            console.log(document.getElementById(`${rows+1}-${column}`));
             document.getElementById(`${rows+1}-${column}`).style.backgroundColor=currentTurn;
             break;
         }
-        
-    }
-   
-
-
-    //board[6][event.target.id]= currentTurn;
-    //document.getElementById(event.target.id).style.backgroundColor=currentTurn;
-} 
+    }  
+}
 
 const circle=document.querySelectorAll(".play");
 circle.forEach((cNum)=>{
-    cNum.addEventListener('click',test)
+    cNum.addEventListener('click',handelPlay)
 })
